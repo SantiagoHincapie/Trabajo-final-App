3cdfac3a4fbbd150121a5e1080153876abd8378d
¨SantiagoHincapie¨ <santiago.augusto48@gmail.com>
2023-09-30 10:21:23 -0500

    Commit del dia

--- Paws/app/src/main/java/com/example/paws/MenuActivities.kt
+++ Paws/app/src/main/java/com/example/paws/MenuActivities.kt
@@ -1,11 +1,28 @@
 package com.example.paws
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivities : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        var buttonEmpezar:Button
+        var intent: Intent
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_activities)
+
+        buttonEmpezar=this.findViewById(R.id.buttonEmpezar)
+
+        buttonEmpezar.setOnClickListener {
+            irA()
+        }
+    }
+
+    fun irA(){
+        intent=Intent(this,LogUpCorreo::class.java)
+        startActivity(intent)
     }
 }
